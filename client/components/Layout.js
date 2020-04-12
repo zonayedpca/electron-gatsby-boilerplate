a754b7a65d7998c58105d5962ea6d59fbce9dee4
Zonayed Ahmed <zonayedpca@yahoo.com>
2020-04-12 19:34:59 +0600

    respective logo added

---
+++ client/components/Layout.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./layout.css";
+
+const Layout = ({ children }) => {
+  return <div className="layout">{children}</div>;
+};
+
+export default Layout;
--- client/screens/index.js
+++ client/screens/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
+import Layout from "../components/Layout";
+
+import gatsbyLogo from "../assets/images/gatsby.svg";
+import electronLogo from "../assets/images/electron.svg";
+
 export default () => {
   const state = useSelector((state) => state);
   console.log(state);
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Layout>
+      <div style={{ textAlign: "center" }}>
+        <div>
+          <img style={{ height: 60 }} alt="Gatsby Logo" src={gatsbyLogo} />
+        </div>
+        <div style={{ fontSize: 30, fontWeight: "bolder", lineHeight: "50px" }}>
+          +
+        </div>
+        <div>
+          <img style={{ height: 45 }} alt="Electron Logo" src={electronLogo} />
+        </div>
+        <p>Open Up file > something</p>
+      </div>
+    </Layout>
   );
 };

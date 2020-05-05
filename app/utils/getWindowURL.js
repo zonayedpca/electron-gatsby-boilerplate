8c23e3d89da14743e2bfc547d2812e04eb32f2d9
Zonayed Ahmed <zonayedpca@yahoo.com>
2020-05-06 00:35:04 +0600

    index fix

--- app/utils/getWindowURL.js
+++ app/utils/getWindowURL.js
@@ -2,9 +2,9 @@ const path = require("path");
 const { is } = require("electron-util");
 
 module.exports = (screen = "", port) => {
-	const indexLocation =
-		screen === "index" ? "index.html" : `${screen}/index.html`;
-	const localPublicPath = `file://${__dirname}/client/public/${indexLocation}`;
+	// const indexLocation =
+	// 	screen === "index" ? "index.html" : `${screen}/index.html`;
+	// const localPublicPath = `file://${__dirname}/client/public/${indexLocation}`;
 	return is.development
 		? `http://localhost:8000/${screen}`
 		: `http://localhost:30000/${screen}`;
--- gatsby-node.js
+++ gatsby-node.js
@@ -2,33 +2,36 @@ const path = require("path");
 const fs = require("fs");
 
 const extractPath = (fileName) => {
-  if (fileName === "index.js") {
-    return "/";
-  }
+	if (fileName === "index.js") {
+		return "/";
+	}
 
-  return fileName.split(".").slice(0, -1).join(".");
+	return fileName.split(".").slice(0, -1).join(".");
 };
 
-const readFiles = (directoryPath) => new Promise((resolve) => {
-  fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-      return new Error("Unable to scan directory");
-    }
+const readFiles = (directoryPath) =>
+	new Promise((resolve) => {
+		fs.readdir(directoryPath, (err, files) => {
+			if (err) {
+				return new Error("Unable to scan directory");
+			}
 
-    return resolve(files);
-  });
-});
+			return resolve(files);
+		});
+	});
 
 exports.createPages = ({ actions }) => {
-  const { createPage } = actions;
-  const screensDir = path.join(__dirname, "client/screens");
-  return readFiles(screensDir)
-    .then((files) => files.forEach((file) => {
-      const component = path.resolve(`client/screens/${file}`);
-      createPage({
-        path: extractPath(file),
-        component,
-      });
-    }))
-    .catch(() => "Something went wrong");
+	const { createPage } = actions;
+	const screensDir = path.join(__dirname, "client/screens");
+	return readFiles(screensDir)
+		.then((files) =>
+			files.forEach((file) => {
+				const component = path.resolve(`client/screens/${file}`);
+				createPage({
+					path: extractPath(file),
+					component,
+				});
+			})
+		)
+		.catch(() => "Something went wrong");
 };

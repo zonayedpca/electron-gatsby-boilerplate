80b86dbd31f389eea5206303b47306c3655c2565
Zonayed Ahmed <zonayedpca@yahoo.com>
2020-05-07 01:24:07 +0600

    Server Promise resolved correctly

--- app/server/index.js
+++ app/server/index.js
@@ -3,25 +3,30 @@ const http = require("http");
 const path = require("path");
 const nodeStatic = require("node-static");
 
-module.exports = (port) =>
-	new Promise((resolve) => {
-		const dir = path.join(__dirname, "..", "public");
-		const file = new nodeStatic.Server(dir);
-		const connection = net.createConnection({ port });
-		connection.on("connect", () => {
-			connection.destroy();
-			getFreePort(port + 1);
-		});
-		connection.on("error", () => {
-			http
-				.createServer((req, res) => {
-					req
-						.addListener("end", function () {
-							file.serve(req, res);
-						})
-						.resume();
-				})
-				.listen(port);
-			resolve({ port, connection });
-		});
+module.exports = (initialPort) => {
+	return new Promise((resolve) => {
+		const getFreePort = (port) => {
+			const dir = path.join(__dirname, "..", "..", "public");
+			const file = new nodeStatic.Server(dir);
+			const connection = net.createConnection({ port });
+			connection.on("connect", () => {
+				connection.destroy();
+				getFreePort(port + 1);
+			});
+			connection.on("error", () => {
+				http
+					.createServer((req, res) => {
+						req
+							.addListener("end", function () {
+								file.serve(req, res);
+							})
+							.resume();
+					})
+					.listen(port, () => {
+						resolve({ port, connection });
+					});
+			});
+		};
+		getFreePort(initialPort);
 	});
+};
--- app/utils/getWindowURL.js
+++ app/utils/getWindowURL.js
@@ -1,7 +1,8 @@
 const { is } = require("electron-util");
 
 module.exports = (screen = "", port) => {
+	console.log(port);
 	return is.development
-		? `http://localhost:8000/${screen}`
+		? `http://localhost:${port}/${screen}`
 		: `http://localhost:${port}/${screen}`;
 };
